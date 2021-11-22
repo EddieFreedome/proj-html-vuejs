7a680cf6ab06827de0af0ebd6fb8b7ce7c471fb7
EddieFreedome <eddie.freed@tutanota.com>
2021-11-22 16:12:14 +0100

    aggiornamenti

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header-container">
+        <div class="header">
+            <img src="src/img/logo-light.png" alt="">
+            <nav class="nav-top">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+                <div class="icon-nav">
+                    <img src="@/assets/svg/svg-0.svg" alt="">
+                    <img src="@/assets/svg/svg-0.svg" alt="">
+                    <img src="@/assets/svg/svg-0.svg" alt="">
+                </div>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.header-container{
+    background-color: aquamarine;
+}
+
+</style>
